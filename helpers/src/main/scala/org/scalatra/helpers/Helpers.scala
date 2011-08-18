37693d9c5aa21108de6d5c8fe2716f33fed51e68
Leif Warner <abimelech@gmail.com>
2011-08-18 14:44:52 -0700

    Some cleanup and comments.

--- helpers/src/main/scala/org/scalatra/helpers/Helpers.scala
+++ helpers/src/main/scala/org/scalatra/helpers/Helpers.scala
@@ -1,19 +1,22 @@
 package org.scalatra
 package helpers
 
-import java.io.PrintWriter
-import javax.servlet.{ServletContext, ServletConfig, FilterConfig}
-import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
-import java.lang.Throwable
+import javax.servlet.http.{HttpServletRequest}
 
 trait Helpers extends ScalatraKernel {
 
-  def header(h:String)(implicit request:HttpServletRequest) = Option( request.getHeader(h) )
+  // Get an incoming HTTP header as an Option (if that header is present)
+  def header(h:String)(implicit request:HttpServletRequest):Option[String] = 
+    Option( request.getHeader(h) )
 
+  // Serve that path with whatever's registered to handle that.
+  // An "internal forward". Useful for serving static files in response to some conditions.
   def yieldRequest(resource:String) { 
     servletContext.getRequestDispatcher(resource).forward(request, response) 
   }
   
+  // Respond to the current request with the servlet "default".
+  // Useful for deffering to static files.
   def yieldRequest {
     servletContext.getNamedDispatcher("default").forward(request, response) 
   }
